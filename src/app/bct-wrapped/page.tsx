cca67eda957c7ab245def53b5cb0681caed58ff9
jackerlus <jack.lewis159@gmail.com>
2024-08-28 22:56:07 +0100

    Added a mechanism to store a user ID in local storage as a test mechanism

--- src/app/bct-wrapped/page.tsx
+++ src/app/bct-wrapped/page.tsx
@@ -1,28 +1,28 @@
 "use client";
+import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 
 export default function Wrapped() {
-  function handleClick() {}
+  const [userId, setUserId] = useState(""); // Store the userId in a state
+
+  // Get the userId from localStorage when the component mounts
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const storedUserId = localStorage.getItem("bctUserId") || "";
+      setUserId(storedUserId);
+    }
+  }, []);
 
   const searchParams = useSearchParams();
   const season = searchParams.get("season");
+
   return (
     <main className="font-sans text-base root m-10 min-h-screen justify-items-center">
-      <div className=" grid justify-items-center">
+      <div className="grid justify-items-center">
         <h1 className="m-5 mt-2 text-4xl">BCT Wrapped</h1>
-        <h2 className="text-xl">Season {searchParams.get("season")}</h2>
-        <input
-          type="text"
-          placeholder="Discord username"
-          id="input-discord"
-          className="p-2 m-3 outline-orange-400 outline outline-2 rounded-md"
-        />
-        <input
-          type="button"
-          value="GET YOUR WRAPPED!"
-          id="button-submit"
-          className="m-3 p-3 max-w-30 max-h-15 font-medium uppercase bg-orange-400 active:bg-orange-400 hover:bg-orange-400/80 shadow-lg rounded-lg transition-all"
-        />
+        <h2 className="text-xl">Season {season}</h2>
+        <h2>{userId ? `User ID: ${userId}` : "No User ID found"}</h2>{" "}
+        {/* Display the userId */}
       </div>
     </main>
   );
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,26 @@
+"use client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Login() {
+  const router = useRouter();
+  let userId: number;
+  userId = Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000;
+  const setUserIdAndRedirect = () => {
+    if (typeof window !== "undefined") {
+      localStorage.setItem("bctUserId", String(userId));
+      router.push("/home");
+    }
+  };
   return (
     <main className="">
-      <Link href="/home">
-        <button className="m-3 p-3 max-w-30 max-h-15 font-medium uppercase text-white bg-red-600 hover:bg-red-600/80 shadow-lg rounded-lg transition-all">
-          Riot login
-        </button>
-      </Link>
+      <button
+        className="m-3 p-3 max-w-30 max-h-15 font-medium uppercase text-white bg-red-600 hover:bg-red-600/80 shadow-lg rounded-lg transition-all"
+        onClick={setUserIdAndRedirect}
+      >
+        Riot login
+      </button>
     </main>
   );
 }
