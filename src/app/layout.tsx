ce322e878d0fce8f56675fb2524ff97e9645a41a
jackerlus <jack.lewis159@gmail.com>
2024-09-07 23:12:01 +0700

    Added footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,17 +10,34 @@ export const metadata: Metadata = {
   description: "Relive your BCT Valorant performance!",
 };
 
+export function Footer() {
+  return (
+    <footer className="bg-gray-200 bottom-0 w-screen flex justify-center">
+      <ul className="list-none">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/privacy">Privacy Policy</Link>
+        </li>
+        <li>
+          <Link href="/tos">Terms of Service</Link>
+        </li>
+      </ul>
+    </footer>
+  );
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html className="" lang="en">
       <body className={inter.className}>
-        <main className="root m-10 min-h-screen justify-items-center text-center">
-          {children}
-        </main>
+        <main className="root m-10 min-h-screen">{children}</main>
+        <Footer />
       </body>
     </html>
   );
