b19847db3c1599d127e7c956074c82cc1b9d169a
jackerlus <jack.lewis159@gmail.com>
2024-09-07 23:12:14 +0700

    Added privacy policy page

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,80 @@
+export default function Privacy() {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+
+      <h2>1. Introduction</h2>
+      <p>
+        Welcome to Boasters Community Tournament (the &quot;Web App&quot;). We
+        value your privacy and are committed to protecting your personal data.
+        This Privacy Policy outlines the types of information we collect, how
+        we use it, and your rights regarding your information.
+      </p>
+
+      <h2>2. Information We Collect</h2>
+      <h3>Riot Account ID</h3>
+      <p>
+        When you use our Web App, we collect your Riot Account ID via Riot Sign
+        On. We do not collect personal information such as your name, email
+        address, or other contact details.
+      </p>
+
+      <h2>3. How We Use Your Information</h2>
+      <p>We use the Riot Account ID for the following purposes:</p>
+      <ul className="mb-2">
+        <li>To provide, maintain, and improve our Web App</li>
+        <li>To personalize your experience within the Web App</li>
+      </ul>
+      <p>
+        We do not use your Riot Account ID for any other purposes without your
+        consent.
+      </p>
+
+      <h2>4. Information Sharing and Disclosure</h2>
+      <p>
+        We do not sell, trade, or otherwise transfer your Riot Account ID to
+        outside parties. We may disclose your information if required to do so
+        by law or in response to valid requests by public authorities (e.g., a
+        court or a government agency).
+      </p>
+
+      <h2>5. Data Security</h2>
+      <p>
+        We implement appropriate technical and organizational measures to
+        protect your Riot Account ID against unauthorized access, alteration,
+        disclosure, or destruction. However, no method of transmission over the
+        Internet or electronic storage is completely secure, and we cannot
+        guarantee absolute security.
+      </p>
+
+      <h2>6. Your Rights</h2>
+      <p>
+        Under the UK GDPR, you have the following rights regarding your data:
+      </p>
+      <ul className="mb-2">
+        <li>The right to access the information we hold about you</li>
+        <li>The right to request correction of inaccurate information</li>
+        <li>The right to request deletion of your data</li>
+        <li>The right to object to the processing of your data</li>
+      </ul>
+      <p>
+        To exercise these rights, please contact us at{" "}
+        <a href="mailto:me@bayfish.dev">me@bayfish.dev</a>.
+      </p>
+
+      <h2>7. Changes to This Privacy Policy</h2>
+      <p>
+        We may update this Privacy Policy from time to time. We will notify you
+        of any changes by posting the new Privacy Policy on our Web App. It is
+        recommended that you review this Privacy Policy periodically for any
+        updates or changes.
+      </p>
+
+      <h2>8. Contact Us</h2>
+      <p>
+        If you have any questions or concerns about this Privacy Policy, please
+        contact us at <a href="mailto:me@bayfish.dev">me@bayfish.dev</a>.
+      </p>
+    </div>
+  );
+}
